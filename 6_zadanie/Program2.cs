e24b75372e94255067a64e759821eb790fd85741
Nikita <crazefire@mail.ru>
2023-04-09 16:29:22 +0300

    6 zadanie end

---
+++ 6_zadanie/Program2.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+double n1 = Convert.ToDouble(a);
+if ((n1 % 2) == 0)
+{
+    Console.Write(n1);
+    Console.WriteLine(" - Число четное");
+}
+else
+{
+    Console.Write(n1);
+    Console.WriteLine(" - Число нечетное");
+};
