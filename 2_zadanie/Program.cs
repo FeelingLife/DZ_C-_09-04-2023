046d848208ff93f3303ceb3b85d525be43e78b33
Nikita <crazefire@mail.ru>
2023-04-09 15:52:07 +0300

    2_zadanie end

--- 2_zadanie/Program.cs
+++ 2_zadanie/Program.cs
@@ -1,5 +1,25 @@
 ﻿using System;
 
 Console.WriteLine("Никита начинает работу:");
-int x = 5;
-int b = 10;
+Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+Convert.ToInt32(a);
+int n1 = Convert.ToInt32(a);
+Console.WriteLine("Введите число: ");
+string b = Console.ReadLine();
+Convert.ToInt32(b);
+int n2 = Convert.ToInt32(b);
+if (n1 > n2)
+{
+    Console.Write(a);
+    Console.WriteLine(" -max");
+    Console.Write(b);
+    Console.WriteLine(" -min");
+}
+else
+{
+    Console.Write(b);
+    Console.WriteLine(" -max");
+    Console.Write(a);
+    Console.WriteLine(" -min");
+};
