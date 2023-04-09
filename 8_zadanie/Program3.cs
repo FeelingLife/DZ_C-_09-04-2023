df35bb137bff7461a12fe74bdcdd1fc7ee66bc1f
Nikita <crazefire@mail.ru>
2023-04-09 17:11:45 +0300

    8 zadanie end

---
+++ 8_zadanie/Program3.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+int n1 = Convert.ToInt32(a);
+int n2 = 0;
+Console.Write("Четные числа: ");
+while (n2 < n1)
+{
+    int v = (n2 + 2);
+    n2 = v;
+    Console.Write(v);
+};
